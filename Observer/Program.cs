e97dde2214700c26b4b02e9ed2b82e6c6e31377c
ryzbaka <hamzaryzvy@gmail.com>
2025-12-28 03:03:50 +0530

    initial file upload

---
+++ Observer/Program.cs
@@ -0,0 +1,63 @@
+﻿public interface IObserver<T>
+{
+  void Update(T value);
+};
+
+public interface IObservable<T>
+{
+  void NotifyObservers(T value);
+  void AddObserver(IObserver<T> o);
+}
+
+public class Enemy: IObserver<string>
+{
+  public void Update(string value)
+  {
+    switch (value)
+    {
+      case "player said hi":
+        Console.WriteLine("Enemy says hello");
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+public class Player:IObservable<string>
+{
+  private List<IObserver<string>> _observers = new List<IObserver<string>>();
+  private static Player instance = new Player();
+  //making constructor private -> see singleton
+  private Player(){}
+  public void NotifyObservers(string value)
+  {
+    foreach (var observer in _observers)
+    {
+       observer.Update(value); 
+    }
+  }
+  public void AddObserver(IObserver <string> o)
+  {
+    _observers.Add(o);
+  }
+  public static Player GetInstance() // see singleton
+  {
+    return instance;
+  }
+  public void SayHello()
+  {
+    Console.WriteLine("Hi");
+    NotifyObservers("player said hi");
+  }
+}
+
+public class Program{
+  public static void Main(string[] args)
+  {
+    var player = Player.GetInstance();
+    var enemy = new Enemy();
+    player.AddObserver(enemy);
+    player.SayHello();
+  }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Singleton{
+  public int Value {get;set;}
+  private static Singleton instance = new Singleton();
+  private Singleton(){}
+  public static Singleton GetInstance()
+  {
+    return instance;
+  }
+};
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    var instance = Singleton.GetInstance();
+    var instance2 = Singleton.GetInstance();
+    instance2.Value = 3;
+    instance.Value = 1;
+    Console.WriteLine($"Value: {instance2.Value}");
+  }
+}
